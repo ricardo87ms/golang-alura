2820501fc8abd585902e59837717dc6d741a1d6d
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2022-12-08 09:30:48 -0300

    Declaração curta de variáveis

--- hello.go
+++ hello.go
@@ -2,18 +2,13 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
-	var nome = "Ricardo"
-	var idade = 35
-	var versao = 2.3
+	nome := "Ricardo"
+	idade := 35
+	versao := 2.3
 
 	fmt.Println("Olá mundo", nome, "sua idade é", idade)
 	fmt.Println("A versão deste programa é", versao)
-
-	fmt.Println("Tipo variável nome", reflect.TypeOf(nome))
-	fmt.Println("Tipo variável idade", reflect.TypeOf(idade))
-	fmt.Println("Tipo variável versao", reflect.TypeOf(versao))
 }
