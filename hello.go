b17ed17325fff1b4c76730e4c825ca0ae4337217
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2022-12-08 09:45:08 -0300

    Criando o menu do sistema de monitoramento

--- hello.go
+++ hello.go
@@ -6,9 +6,23 @@ import (
 
 func main() {
 	nome := "Ricardo"
-	idade := 35
 	versao := 2.3
 
-	fmt.Println("Olá mundo", nome, "sua idade é", idade)
+	fmt.Println("Olá", nome, ", bem vindo ao sitema de monitoramento.")
 	fmt.Println("A versão deste programa é", versao)
+
+	fmt.Println("Escolha uma das opções abaixo:")
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do sistema")
+
+	var comando int
+
+	// No Scanf é necessário informar o tipo a ser utilizado para variável comando
+	// fmt.Scanf("%d", &comando)
+
+	fmt.Scan(&comando)
+
+	fmt.Println("O endereço de memória da variável comando é", &comando)
+	fmt.Println("O comando digitado foi", comando)
 }
