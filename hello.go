e0536b03c091291d054f8d3c768929c10524423c
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2022-12-08 09:28:13 -0300

    Inferência de tipos na declaração de variáveis GO

--- hello.go
+++ hello.go
@@ -1,12 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	var nome string = "Ricardo"
-	var idade int = 35
-	var versao float32 = 2.3
+	var nome = "Ricardo"
+	var idade = 35
+	var versao = 2.3
 
 	fmt.Println("Olá mundo", nome, "sua idade é", idade)
 	fmt.Println("A versão deste programa é", versao)
+
+	fmt.Println("Tipo variável nome", reflect.TypeOf(nome))
+	fmt.Println("Tipo variável idade", reflect.TypeOf(idade))
+	fmt.Println("Tipo variável versao", reflect.TypeOf(versao))
 }
