ce8fd093f763ebd7d704800be7e255b5e530ee67
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2022-12-08 10:32:09 -0300

    Criando funções para separações de códigos

--- hello.go
+++ hello.go
@@ -2,37 +2,54 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	nome := "Ricardo"
-	versao := 2.3
 
-	fmt.Println("Olá", nome, ", bem vindo ao sitema de monitoramento.")
-	fmt.Println("A versão deste programa é", versao)
+	exibeIntroducao()
+	exibeMenu()
 
-	fmt.Println("Escolha uma das opções abaixo:")
-	fmt.Println("1 - Iniciar monitoramento")
-	fmt.Println("2 - Exibir logs")
-	fmt.Println("0 - Sair do sistema")
-
-	var comando int
-
-	fmt.Scan(&comando)
+	comando := leComando()
 
 	switch comando {
 
 	case 1:
 		fmt.Println("Monitorando...")
-
+		break
 	case 2:
 		fmt.Println("Exibindo Logs...")
 
 	case 0:
 		fmt.Println("Saindo do Sistema...")
+		os.Exit(0)
 
 	default:
 		fmt.Println("Não conheço o seu comando...")
+		os.Exit(-1)
 	}
+}
 
+func exibeIntroducao() {
+	nome := "Ricardo"
+	versao := 2.3
+	fmt.Println("Olá", nome, ", bem vindo ao sitema de monitoramento.")
+	fmt.Println("A versão deste programa é", versao)
+	fmt.Println()
+}
+
+func exibeMenu() {
+	fmt.Println("Escolha uma das opções abaixo:")
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do sistema")
+}
+
+func leComando() int {
+
+	var comandoLido int
+
+	fmt.Scan(&comandoLido)
+
+	return comandoLido
 }
