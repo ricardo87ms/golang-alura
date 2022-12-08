b1c96ddfddfd7f6236a8d2610299548efdbd810e
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2022-12-08 09:25:03 -0300

    Commit inicil curso Golang Alura

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome string = "Ricardo"
+	var idade int = 35
+	var versao float32 = 2.3
+
+	fmt.Println("Olá mundo", nome, "sua idade é", idade)
+	fmt.Println("A versão deste programa é", versao)
+}
