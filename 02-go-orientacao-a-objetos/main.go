428ab69636ab43eac875c3eba6d45da0c4095ac0
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2022-12-27 19:43:05 -0300

    Criando a minha primeira struct

--- 02-go-orientacao-a-objetos/main.go
+++ 02-go-orientacao-a-objetos/main.go
@@ -1,1 +1,22 @@
 package main
+
+import "fmt"
+
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
+func main() {
+
+	// var contaRicardo ContaCorrente = ContaCorrente{}
+	// contaRicardo := ContaCorrente{titular: "Ricardo", numeroAgencia: 222, numeroConta: 111111, saldo: 150.5}
+	contaRicardo := ContaCorrente{titular: "Ricardo", saldo: 150.5}
+
+	contaTatiele := ContaCorrente{"Tatiele", 222, 1111, 220.3}
+
+	fmt.Println(contaRicardo)
+	fmt.Println(contaTatiele)
+}
