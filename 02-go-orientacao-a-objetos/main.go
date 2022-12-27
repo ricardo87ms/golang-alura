cea98ef81b6e2ed80b3c57f7a017999948b1e293
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2022-12-27 20:10:37 -0300

    Entendendo a criação de ponteiros

--- 02-go-orientacao-a-objetos/main.go
+++ 02-go-orientacao-a-objetos/main.go
@@ -13,10 +13,23 @@ func main() {
 
 	// var contaRicardo ContaCorrente = ContaCorrente{}
 	// contaRicardo := ContaCorrente{titular: "Ricardo", numeroAgencia: 222, numeroConta: 111111, saldo: 150.5}
-	contaRicardo := ContaCorrente{titular: "Ricardo", saldo: 150.5}
+	// contaRicardo := ContaCorrente{titular: "Ricardo", saldo: 150.5}
 
 	contaTatiele := ContaCorrente{"Tatiele", 222, 1111, 220.3}
 
-	fmt.Println(contaRicardo)
+	// fmt.Println(contaRicardo)
 	fmt.Println(contaTatiele)
+
+	var contaAna *ContaCorrente
+	contaAna = new(ContaCorrente)
+	contaAna.titular = "Ana"
+	contaAna.saldo = 52.3
+
+	var contaRicardo *ContaCorrente
+	contaRicardo = new(ContaCorrente)
+	contaRicardo.titular = "Ricardo"
+	contaRicardo.saldo = 60.30
+
+	fmt.Println(*contaAna)
+	fmt.Println(*contaRicardo)
 }
