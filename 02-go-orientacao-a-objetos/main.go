34885de53616c1467584192ba25545af57fdb370
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2023-01-14 08:28:31 -0300

    Comparanção de structs

--- 02-go-orientacao-a-objetos/main.go
+++ 02-go-orientacao-a-objetos/main.go
@@ -16,9 +16,12 @@ func main() {
 	// contaRicardo := ContaCorrente{titular: "Ricardo", saldo: 150.5}
 
 	contaTatiele := ContaCorrente{"Tatiele", 222, 1111, 220.3}
+	contaTatiele2 := ContaCorrente{"Tatiele", 222, 1111, 220.3}
 
 	// fmt.Println(contaRicardo)
-	fmt.Println(contaTatiele)
+	// Comparando na declaração do Go
+	fmt.Println(contaTatiele, contaTatiele2)
+	fmt.Println(contaTatiele == contaTatiele2)
 
 	var contaAna *ContaCorrente
 	contaAna = new(ContaCorrente)
@@ -30,6 +33,16 @@ func main() {
 	contaRicardo.titular = "Ricardo"
 	contaRicardo.saldo = 60.30
 
-	fmt.Println(*contaAna)
-	fmt.Println(*contaRicardo)
+	var contaRicardo2 *ContaCorrente
+	contaRicardo2 = new(ContaCorrente)
+	contaRicardo2.titular = "Ricardo"
+	contaRicardo2.saldo = 60.30
+
+	// Comparando na declaração normal do Go
+	fmt.Println(contaRicardo, contaRicardo2)
+	fmt.Println(contaRicardo == contaRicardo2)
+	fmt.Println(*contaRicardo == *contaRicardo2)
+
+	//fmt.Println(*contaAna)
+	//fmt.Println(*contaRicardo)
 }
