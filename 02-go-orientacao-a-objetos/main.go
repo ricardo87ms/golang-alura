203a51ad58865ce340804038b6192ad4bddab618
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2023-03-09 08:06:38 -0300

    Criando o método depositar

--- 02-go-orientacao-a-objetos/main.go
+++ 02-go-orientacao-a-objetos/main.go
@@ -21,11 +21,21 @@ func (c *ContaCorrente) Sacar(valorSaque float64) string {
 	return "Valor sacado com sucesso"
 }
 
+func (c *ContaCorrente) Depositar(valorDeposito float64) (string, float64) {
+	if valorDeposito < 0 {
+		return "Não é possível depositar valores negativos!!!", c.saldo
+	}
+
+	c.saldo += valorDeposito
+
+	return "Valor depositado com sucesso!!", c.saldo
+}
+
 func main() {
 
 	contaRicardo := ContaCorrente{"Ricardo", 222, 333, 300}
 
 	fmt.Println(contaRicardo.saldo)
-	fmt.Println(contaRicardo.Sacar(-200))
+	fmt.Println(contaRicardo.Depositar(-200))
 	fmt.Println(contaRicardo.saldo)
 }
